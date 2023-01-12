3cd710f747abab5f11811be3ad59b1a0d670f978
Stanislav (Stas) Katkov <contact@skatkov.com>
2023-01-12 14:56:13 +0100

    remove branch coverage metric: (#2)

--- dist/index.js
+++ dist/index.js
@@ -1625,8 +1625,7 @@ function trimWorkspacePath(filename) {
 function formatDiff(diff) {
     return [
         trimWorkspacePath(diff.filename),
-        formatDiffItem(diff.lines),
-        formatDiffItem(diff.branches)
+        formatDiffItem(diff.lines)
     ];
 }
 function run() {
@@ -1657,7 +1656,7 @@ function run() {
             }
             else {
                 content = markdown_table_1.default([
-                    ['Filename', 'Lines', 'Branches'],
+                    ['Filename', 'Lines'],
                     ...diff.map(formatDiff)
                 ]);
             }
@@ -9784,25 +9783,6 @@ function linesCoverage(coverage) {
     const covered = effectiveLines.filter(hit => hit > 0).length;
     return floor((covered / rows) * 100, 2);
 }
-function branchesCoverages(coverage) {
-    const conditions = Object.keys(coverage);
-    if (conditions.length === 0) {
-        return 100;
-    }
-    let total = 0;
-    let covered = 0;
-    for (const k of conditions) {
-        const cond = coverage[k];
-        for (const branch of Object.keys(cond)) {
-            total += 1;
-            const hit = cond[branch];
-            if (hit > 0) {
-                covered += 1;
-            }
-        }
-    }
-    return floor((covered / total) * 100, 2);
-}
 class Coverage {
     constructor(resultset) {
         const coverages = resultset['Minitest']['coverage'];
@@ -9811,8 +9791,7 @@ class Coverage {
             const coverage = coverages[filename];
             this.files.push({
                 filename,
-                lines: linesCoverage(coverage.lines),
-                branches: branchesCoverages(coverage.branches)
+                lines: linesCoverage(coverage.lines)
             });
         }
     }
@@ -9858,9 +9837,6 @@ function isDifference(cov1, cov2) {
     if (cov1.lines !== cov2.lines) {
         return true;
     }
-    if (cov1.branches !== cov2.branches) {
-        return true;
-    }
     return false;
 }
 function makeDiff(cov1, cov2) {
@@ -9870,21 +9846,18 @@ function makeDiff(cov1, cov2) {
     if (!cov1 && cov2) {
         return {
             filename: cov2.filename,
-            lines: { from: null, to: cov2.lines },
-            branches: { from: null, to: cov2.branches }
+            lines: { from: null, to: cov2.lines }
         };
     }
     if (!cov2 && cov1) {
         return {
             filename: cov1.filename,
-            lines: { from: cov1.lines, to: null },
-            branches: { from: cov1.branches, to: null }
+            lines: { from: cov1.lines, to: null }
         };
     }
     return {
         filename: cov1.filename,
-        lines: { from: cov1.lines, to: cov2.lines },
-        branches: { from: cov1.branches, to: cov2.branches }
+        lines: { from: cov1.lines, to: cov2.lines }
     };
 }
 
--- src/simplecov.ts
+++ src/simplecov.ts
@@ -10,21 +10,13 @@ type RawCoverages = {
 
 type RawCoverage = {
   lines: LineCoverage
-  branches: BranchCoverage
 }
 
 type LineCoverage = (number | null)[]
 
-type BranchCoverage = {
-  [condition: string]: {
-    [branch: string]: number
-  }
-}
-
 type FileCoverage = {
   filename: string
   lines: number
-  branches: number
 }
 
 function floor(n: number, digits = 0): number {
@@ -44,27 +36,6 @@ function linesCoverage(coverage: LineCoverage): number {
   return floor((covered / rows) * 100, 2)
 }
 
-function branchesCoverages(coverage: BranchCoverage): number {
-  const conditions = Object.keys(coverage)
-  if (conditions.length === 0) {
-    return 100
-  }
-
-  let total = 0
-  let covered = 0
-  for (const k of conditions) {
-    const cond = coverage[k]
-    for (const branch of Object.keys(cond)) {
-      total += 1
-      const hit = cond[branch]
-      if (hit > 0) {
-        covered += 1
-      }
-    }
-  }
-  return floor((covered / total) * 100, 2)
-}
-
 export class Coverage {
   files: FileCoverage[]
 
@@ -75,8 +46,7 @@ export class Coverage {
       const coverage = coverages[filename]
       this.files.push({
         filename,
-        lines: linesCoverage(coverage.lines),
-        branches: branchesCoverages(coverage.branches)
+        lines: linesCoverage(coverage.lines)
       })
     }
   }
@@ -127,19 +97,12 @@ function isDifference(cov1?: FileCoverage, cov2?: FileCoverage): boolean {
   if (cov1!.lines !== cov2!.lines) {
     return true
   }
-  if (cov1!.branches !== cov2!.branches) {
-    return true
-  }
   return false
 }
 
 export type FileCoverageDiff = {
   filename: string
   lines: {
-    from: number | null
-    to: number | null
-  }
-  branches: {
     from: number | null
     to: number | null
   }
@@ -153,20 +116,17 @@ function makeDiff(cov1?: FileCoverage, cov2?: FileCoverage): FileCoverageDiff {
   if (!cov1 && cov2) {
     return {
       filename: cov2.filename,
-      lines: {from: null, to: cov2.lines},
-      branches: {from: null, to: cov2.branches}
+      lines: {from: null, to: cov2.lines}
     }
   }
   if (!cov2 && cov1) {
     return {
       filename: cov1.filename,
-      lines: {from: cov1.lines, to: null},
-      branches: {from: cov1.branches, to: null}
+      lines: {from: cov1.lines, to: null}
     }
   }
   return {
     filename: cov1!.filename,
-    lines: {from: cov1!.lines, to: cov2!.lines},
-    branches: {from: cov1!.branches, to: cov2!.branches}
+    lines: {from: cov1!.lines, to: cov2!.lines}
   }
 }
